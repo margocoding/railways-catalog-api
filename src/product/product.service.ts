61f602bb553173534eb2e23338b14d8b93d7507d
flofeyka <danilbashirov0@gmail.com>
2026-08-25 06:16:06 +0500

    Initial commit

---
+++ src/product/product.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'prisma/prisma.service';
+
+@Injectable()
+export class ProductService {
+    constructor(private readonly prisma: PrismaService) {}
+
+    
+}
